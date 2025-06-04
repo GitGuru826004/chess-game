64b595b729080612ae7396203c11bcf976b6e537
Anupam <anupamgarg2005@gmail.com>
2025-06-05 04:45:25 +0530

    third commit

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
+//images
+app.use(express.static('assets'));
 // Routes
 app.use("/api/auth", authRoutes);
 
